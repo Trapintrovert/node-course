32bc03f2baf56ff6a2628c207d51eed7496e7946
trapintrovert <mcneo062@gmail.com>
2023-02-14 13:21:50 +0100

    Node course

---
+++ notes-app/app.js
@@ -0,0 +1,52 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
+
+// Customize yargs version
+yargs.version('1.1.0');
+
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: function (argv) {
+    console.log('Adding a new note!', argv);
+  },
+});
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Removing the note');
+  },
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function () {
+    console.log('Read the note');
+  },
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List a note',
+  handler: function () {
+    console.log('List the notes');
+  },
+});
+
+//add, remove, read, list
+
+console.log(yargs.argv);
