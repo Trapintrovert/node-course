b956c625ecbcd04bbe6f8de600bed3e0540a4444
trapintrovert <mcneo062@gmail.com>
2023-02-15 23:29:40 +0100

    modification

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,6 +4,13 @@ const yargs = require('yargs');
 // Customize yargs version
 yargs.version('1.1.0');
 
+// Challenge: Add an option to yargs
+//
+// 1. Setup a body option for add command
+// 2. Configure a description, make it required, and for it to be a string
+// 3. Log the body value in the handler function
+// 4. Test your work!
+
 // Create add command
 yargs.command({
   command: 'add',
@@ -14,9 +21,15 @@ yargs.command({
       demandOption: true,
       type: 'string',
     },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    },
   },
   handler: function (argv) {
-    console.log('Adding a new note!', argv);
+    console.log('Title: ' + argv.title);
+    console.log('Body: ' + argv.body);
   },
 });
 
@@ -50,3 +63,4 @@ yargs.command({
 //add, remove, read, list
 
 console.log(yargs.argv);
+yargs.parse;
