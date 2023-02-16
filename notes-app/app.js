ccf72a1eac554c6903257f2f987e3ea253c9f248
trapintrovert <mcneo062@gmail.com>
2023-02-16 13:21:32 +0100

    section 4, E20

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk');
 const yargs = require('yargs');
+const notes = require('./notes.js');
 
 // Customize yargs version
 yargs.version('1.1.0');
@@ -28,8 +29,7 @@ yargs.command({
     },
   },
   handler: function (argv) {
-    console.log('Title: ' + argv.title);
-    console.log('Body: ' + argv.body);
+    notes.addNote(argv.title, argv.body);
   },
 });
 
