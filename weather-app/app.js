0f4471341c120ab9525916c72c7d0a7951c041fe
trapintrovert <mcneo062@gmail.com>
2023-02-21 11:43:56 +0100

    S6, E32

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,10 @@
-console.log('Starting');
+const request = require('postman-request');
 
-setTimeout(() => {
-  console.log('2 Second Timer');
-}, 2000);
+const url =
+  'http://api.weatherstack.com/current?access_key=e0884768dda46ea977def2f62abbddb9&query=37.8267,-122.4233&units=f';
 
-setTimeout(() => {
-  console.log('0 Second Timer');
-}, 0);
-
-console.log('Stopping');
+request({ url: url, json: true }, (error, response) => {
+  console.log(
+    `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out. `
+  );
+});
