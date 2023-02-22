2e6defe79fac1aeb4bb8457435974380fa176185
trapintrovert <mcneo062@gmail.com>
2023-02-22 01:26:31 +0100

    S6, E35

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,13 +1,19 @@
 const request = require('postman-request');
 
-const url =
-  'http://api.weatherstack.com/current?access_key=e0884768dda46ea977def2f62abbddb9&query=37.8267,-122.4233&units=f';
+// const url =
+//   'http://api.weatherstack.com/current?access_key=e0884768dda46ea977def2f62abbddb9&query=37.8267,-122.4233&units=f';
 
-request({ url: url, json: true }, (error, response) => {
-  console.log(
-    `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out. `
-  );
-});
+// request({ url: url, json: true }, (error, response) => {
+//   if (error) {
+//     console.log('Unable to connect to weather service!');
+//   } else if (response.body.error) {
+//     console.log('Unable to find location');
+//   } else {
+//     console.log(
+//       `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out. `
+//     );
+//   }
+// });
 
 // Geocoding
 // Address -> Lat/Log -> Weather
@@ -16,8 +22,14 @@ const geocodeUrl =
   'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidHJhcGludHJvdmVydCIsImEiOiJjazIzdmlmb2IwZWV3M2JyZTZvN3A3ZWxmIn0.tBmFIOC_BNtDE0HaJy2T6w&limit=1';
 
 request({ url: geocodeUrl, json: true }, (error, response) => {
-  const latitude = response.body.features[0].center[1];
-  const longitude = response.body.features[0].center[0];
+  if (error) {
+    console.log('Unable to connect to location service!');
+  } else if (response.body.features.length === 0) {
+    console.log('Unable to find location. Try another search');
+  } else {
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
 
-  console.log(latitude, longitude);
+    console.log(latitude, longitude);
+  }
 });
