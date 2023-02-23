6dfefabd63f1307f72f4b77421a51932b54c4e24
trapintrovert <mcneo062@gmail.com>
2023-02-23 14:14:52 +0100

    S6, E44

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,31 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-  res.send('Hello express!');
+  res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req, res) => {
-  res.send('Help page');
+  res.send([
+    {
+      name: 'Andrew',
+      age: 27,
+    },
+    {
+      name: 'Sara',
+      age: 27,
+    },
+  ]);
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.send('<h1>About page</h1>');
 });
 
 app.get('/weather', (req, res) => {
-  res.send('Weather page');
+  res.send({
+    forecast: '50 degrees',
+    location: 'Lagos',
+  });
 });
 
 app.listen(3000, () => {
