cd8681b8d25eb3245dd7e2d784a53eff58f5a959
trapintrovert <mcneo062@gmail.com>
2023-02-26 17:14:19 +0100

    S8, E57

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log('Client side javascript file loaded!');
+
+fetch('http://localhost:3000/weather?address=lagos').then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(data.forecast.description[0]);
+    }
+  });
+});
