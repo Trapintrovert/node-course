aab985bc71fd8cc497b92e4448fac4a95e578541
trapintrovert <mcneo062@gmail.com>
2023-02-26 17:29:04 +0100

    S8, E58

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,23 @@
 console.log('Client side javascript file loaded!');
 
-fetch('http://localhost:3000/weather?address=lagos').then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast.description[0]);
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const location = search.value;
+
+  fetch('http://localhost:3000/weather?address=' + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          console.log(data.error);
+        } else {
+          console.log(data.location);
+          console.log(data.forecast.description[0]);
+        }
+      });
     }
-  });
+  );
+  // console.log(location);
 });
