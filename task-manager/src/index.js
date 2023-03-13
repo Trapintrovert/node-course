4972edcdc7f6ebe2f855add9757e7403f8824399
trapintrovert <mcneo062@gmail.com>
2023-03-13 17:42:54 +0100

    S11, E92

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,11 +14,35 @@ app.post('/users', (req, res) => {
   user
     .save()
     .then(() => {
-      res.send(user);
+      res.status(201).send(user);
     })
     .catch((e) => {
       re.status(400).send(e);
     });
+});
+
+app.get('/users', (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id;
+
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(401).send();
+      }
+
+      res.send(user);
+    })
+    .catch((e) => {});
 });
 
 app.post('/tasks', (req, res) => {
@@ -26,10 +50,36 @@ app.post('/tasks', (req, res) => {
   task
     .save()
     .then(() => {
-      res.send(task);
+      res.status(201).send(task);
     })
     .catch((e) => {
       res.status(400).send(e);
+    });
+});
+
+app.get('/tasks', (req, res) => {
+  Task.find({})
+    .then((task) => {
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
+app.get('/tasks/:id', (res, res) => {
+  const _id = req.params.id;
+
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(500).send();
     });
 });
 
