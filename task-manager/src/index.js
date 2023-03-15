1bd560a1269016aa72c7fe32656be4d7d87ecd87
trapintrovert <mcneo062@gmail.com>
2023-03-15 13:31:59 +0100

    S11, E99

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -50,7 +50,7 @@ app.patch('/users/:id', async (req, res) => {
     allowedUpdates.includes(update)
   );
   if (!isValidOperation) {
-    res.status(400).send({ error: 'Invalid updates!' });
+    return res.status(400).send({ error: 'Invalid updates!' });
   }
 
   try {
@@ -95,6 +95,31 @@ app.get('/tasks/:id', async (res, res) => {
     res.send(task);
   } catch (e) {
     res.status(500).send();
+  }
+});
+
+app.patch('/tasks/:id', async (req, res) => {
+  const _id = req.params.id;
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['description', 'completed'];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) return res.status(400).send();
+
+    res.send(task);
+  } catch (e) {
+    res.status(400).send(e);
   }
 });
 
