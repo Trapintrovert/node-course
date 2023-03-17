b2a057c905b459d980a5054f8c529171bd0a19a6
trapintrovert <mcneo062@gmail.com>
2023-03-17 13:48:48 +0100

    S11, E110

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
-
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
@@ -15,7 +15,7 @@ router.post('/users', async (req, res) => {
   }
 });
 
-router.post('/users/login', async (req, res) => {
+router.post('/users/login', auth, async (req, res) => {
   try {
     const user = await User.findByCredentials(
       req.body,
@@ -30,13 +30,8 @@ router.post('/users/login', async (req, res) => {
   }
 });
 
-router.get('/users', async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (e) {
-    res.status(500).send();
-  }
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get('/users/:id', async (req, res) => {
