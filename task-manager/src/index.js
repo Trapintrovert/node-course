3b6d6a3afbfe45b0ad6832590e7e722a18f697ba
trapintrovert <mcneo062@gmail.com>
2023-03-17 16:01:26 +0100

    S11, E112

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -26,16 +26,8 @@ app.listen(port, () => {
   console.log('Server is up on port ' + port);
 });
 
-const jwt = require('jsonwebtoken');
+// const pet = {
+//   name: 'Hal',
+// };
 
-const myFunction = async () => {
-  const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', {
-    expiresIn: '7 days',
-  });
-  console.log(token);
-
-  const data = jwt.verify(token, 'thisismynewcourse');
-  console.log(data);
-};
-
-myFunction();
+// console.log(JSON.stringify(pet));
