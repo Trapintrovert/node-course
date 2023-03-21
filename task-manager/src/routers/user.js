992e478a5992b2e09c5f10f4c9b0257d68aa0165
trapintrovert <mcneo062@gmail.com>
2023-03-21 10:55:30 +0100

    S11, E123

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -100,8 +100,16 @@ const upload = multer({
   },
 });
 
-router.post('/users/me/avatar', auth, upload.single('avatar'), (req, res) => {
-  res.send();
-});
+router.post(
+  '/users/me/avatar',
+  auth,
+  upload.single('avatar'),
+  (req, res) => {
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
 module.exports = router;
