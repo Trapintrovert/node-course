df62c4275d3f40569337223a09dfa9ba1197140e
trapintrovert <mcneo062@gmail.com>
2023-04-28 16:22:51 +0100

    S17, E168

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -26,6 +26,9 @@ io.on('connection', (socket) => {
     socket.broadcast
       .to(room)
       .emit('message', generateMessage(`${username} has joined!`));
+
+    // socket.emit, io.emit, socket.broadcast.emit
+    // io.to.emit, socket.broadcast.to.emit
   });
 
   socket.on('sendMessage', (message, callback) => {
